4224812efb5e5d1812388df0435613a16587ff0d
Luismoteando <LuisMiguel.Ortiz1@alu.uclm.es>
2019-01-28 18:10:25 +0100

    fifteenth commit

--- streaming/js/streaming.js
+++ streaming/js/streaming.js
@@ -67,10 +67,18 @@ function changeVideo(response) {
       }, 500);
     }
   } else {
-    streaming_odd.src = "media/video" + response[1].value + "/opcion" + response[3].value + ".mp4";
-    setTimeout(function() {
-      streaming_even.style.display = "none";
-      streaming_odd.style.display = "block";
-    }, 500);
+    if(video % 2 == 0) {
+      streaming_odd.src = "media/video" + response[1].value + "/opcion" + response[3].value + ".mp4";
+      setTimeout(function() {
+        streaming_even.style.display = "none";
+        streaming_odd.style.display = "block";
+      }, 500);
+    } else {
+      streaming_even.src = "media/video" + response[1].value + "/opcion" + response[3].value + ".mp4";
+      setTimeout(function() {
+        streaming_odd.style.display = "none";
+        streaming_even.style.display = "block";
+      }, 500);
+    }
   }
 }
