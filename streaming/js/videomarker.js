78e6168c00b3ef9a585b735b53182491e92d9d22
Luismoteando <LuisMiguel.Ortiz1@alu.uclm.es>
2019-01-28 13:14:44 +0100

    third commit

--- streaming/js/videomarker.js
+++ streaming/js/videomarker.js
@@ -2,7 +2,6 @@ var score = document.getElementById("score");
 var videomarker = 0;
 
 $(document).ready(function () {
-  readTextFile("/media/pi/DATA/config.txt");
   runPoll();
 });
 
@@ -20,20 +19,3 @@ function runPoll() {
     })
   }, 100);
 };
-
-function readTextFile(file) {
-  var rawFile = new XMLHttpRequest();
-  rawFile.open("GET", file, false);
-  rawFile.onreadystatechange = function ()
-  {
-    if(rawFile.readyState === 4)
-    {
-      if(rawFile.status === 200 || rawFile.status == 0)
-      {
-        var allText = rawFile.responseText;
-        alert(allText);
-      }
-    }
-  }
-  rawFile.send(null);
-}
