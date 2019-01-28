2a2bd5194550a982cba03aea58d4c07fabab70ca
Luismoteando <LuisMiguel.Ortiz1@alu.uclm.es>
2019-01-28 12:59:10 +0100

    first commit

---
+++ streaming/js/streaming.js
@@ -0,0 +1,67 @@
+var streaming_even = document.getElementById("streaming_even");
+var streaming_odd = document.getElementById("streaming_odd");
+var state = 0;
+
+$(document).ready(function () {
+  runPoll();
+});
+
+function runPoll() {
+  setTimeout(function () {
+    $.ajax({
+      url: 'http://localhost:8888/server/server.php',
+      success: function (response) {
+        checkPlay(response[0]);
+        if(state != response[1].value) {
+          changeState(response);
+        }
+      },
+      dataType: "json",
+      complete: function () {
+        runPoll();
+      }
+    })
+  }, 100);
+};
+
+function checkPlay(response) {
+  if(streaming_even.style.display != "none") {
+    if(response.value == "true") {
+      if(streaming_even.paused) {
+        streaming_even.play();
+      }
+    } else if(response.value == "false"){
+      if(!streaming_even.paused) {
+        streaming_even.pause();
+      }
+    }
+  } else if(streaming_odd.style.display != "none") {
+    if(response.value == "true") {
+      if(streaming_odd.paused) {
+        streaming_odd.play();
+      }
+    } else if(response.value == "false"){
+      if(!streaming_odd.paused) {
+        streaming_odd.pause();
+      }
+    }
+  }
+}
+
+function changeState(response) {
+  video = response[1].value;
+  opcion = response[2].value;
+  if(opcion % 2 == 0 && video % 2 != 0) {
+    streaming_even.src = "video" + response[1].value + "/opcion" + response[2].value + ".mp4";
+    setTimeout(function() {
+      streaming_odd.style.display = "none";
+      streaming_even.style.display = "block";
+    }, 500);
+  } else {
+    streaming_odd.src = "video" + response[1].value + "/opcion" + response[2].value + ".mp4";
+    setTimeout(function() {
+      streaming_even.style.display = "none";
+      streaming_odd.style.display = "block";
+    }, 500);
+  }
+}
---
+++ streaming/js/videomarker.js
@@ -0,0 +1,39 @@
+var score = document.getElementById("score");
+var videomarker = 0;
+
+$(document).ready(function () {
+  readTextFile("/media/pi/DATA/config.txt");
+  runPoll();
+});
+
+function runPoll() {
+  setTimeout(function () {
+    $.ajax({
+      url: 'http://localhost:8888/server/server.php',
+      success: function (response) {
+        score.innerHTML = response[2].value;
+      },
+      dataType: "json",
+      complete: function () {
+        runPoll();
+      }
+    })
+  }, 100);
+};
+
+function readTextFile(file) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function ()
+  {
+    if(rawFile.readyState === 4)
+    {
+      if(rawFile.status === 200 || rawFile.status == 0)
+      {
+        var allText = rawFile.responseText;
+        alert(allText);
+      }
+    }
+  }
+  rawFile.send(null);
+}
