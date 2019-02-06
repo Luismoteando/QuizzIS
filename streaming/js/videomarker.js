af44a6a3358c120727abb5e5a486adcf523538e4
Luismoteando <LuisMiguel.Ortiz1@alu.uclm.es>
2019-02-06 19:55:37 +0100

    twenty-third commit

--- streaming/js/videomarker.js
+++ streaming/js/videomarker.js
@@ -1,8 +1,10 @@
 var score = document.getElementById("score");
 var videomarker = 0;
-
+var team;
 $(document).ready(function () {
   runPoll();
+  team = getParameterByName("team");
+  document.getElementById("team").innerHTML = team;
 });
 
 function runPoll() {
@@ -19,3 +21,19 @@ function runPoll() {
     })
   }, 100);
 };
+
+function getParameterByName(name) {
+  name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  var regexS = "[\\?&]"+name+"=([^&#]*)";
+  var regex = new RegExp( regexS );
+  var results = regex.exec( window.location.href );
+  if(results == null) {
+    return "";
+  } else {
+    if ((results[1].indexOf('?')) > 0) {
+      return decodeURIComponent(results[1].substring(0,results[1].indexOf('?')).replace(/\+/g, " "));
+    } else {
+      return decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+  }
+}
