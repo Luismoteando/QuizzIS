b3c93f8b740174762e0009a1ea960bde932acb2f
Luismoteando <LuisMiguel.Ortiz1@alu.uclm.es>
2019-02-06 20:21:11 +0100

    twenty-sixth commit

--- role.sh
+++ role.sh
@@ -9,6 +9,6 @@ while IFS="\n" read -r role team; do
   if [ $role == "streaming" ] ; then
     /usr/bin/chromium-browser --kiosk --disable-restore-session-state --app="http://localhost/streaming/streaming.html"
   elif [ $role == "videomarker" ] ; then
-    /usr/bin/chromium-browser --kiosk --disable-restore-session-state --app="http://localhost/streaming/videomarker?team=$team.html"
+    /usr/bin/chromium-browser --kiosk --disable-restore-session-state --app="http://localhost/streaming/videomarker.html?team=$team"
   fi
 done < "$file"
