bec964b08c841a4bb9e8ede68d717163ff0f3680
Luismoteando <LuisMiguel.Ortiz1@alu.uclm.es>
2019-03-14 10:01:54 +0100

    fifty-fifth commit

--- pushbutton/raspduino.py
+++ pushbutton/raspduino.py
@@ -4,9 +4,9 @@ import time
 arduino = serial.Serial('/dev/ttyACM0', 9600)
 
 while 1:
-    # if(arduino.in_waiting > 0):
+    if(arduino.in_waiting > 0):
         x = arduino.readline()
         print x
-        time.sleep(30)
-        
+        time.sleep(10)
+
 arduino.close()
