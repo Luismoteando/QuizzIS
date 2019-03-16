7b18e583d149e240e3982530fa7bdc910ba9f6fb
Luismoteando <LuisMiguel.Ortiz1@alu.uclm.es>
2019-03-16 17:22:58 +0100

    sixty-second commit

--- pushbutton/raspduino.py
+++ pushbutton/raspduino.py
@@ -6,7 +6,7 @@ arduino = serial.Serial('/dev/ttyACM0', 9600)
 
 while 1:
     if(arduino.in_waiting > 0):
-        x = arduino.readline().decode('utf-8')
+        x = arduino.readline().strip()
         data = {'turn': x}
         requests.post(url = "http://olistreaming.ddns.net:8888/server/bachServer.php", data = data)
 
