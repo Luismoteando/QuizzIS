3f4754c0b0d6443c7e4ae96ca5c133890ddc6a86
Luismoteando <LuisMiguel.Ortiz1@alu.uclm.es>
2019-03-21 12:41:21 +0100

    seventy-sixth commit

--- server/cyclServer.php
+++ server/cyclServer.php
@@ -7,9 +7,35 @@ $client = new MongoDB\Client;
 $db = $client->streaming;
 $collection = $db->cyclStreaming;
 
+$play = $collection->findOne(
+  ['_id' => 'play']
+);
+$state = $collection->findOne(
+  ['_id' => 'state']
+);
+$teamA = $collection ->findOne(
+  ['_id' => 'teamA']
+);
+$teamB = $collection ->findOne(
+  ['_id' => 'teamB']
+);
+$teamC = $collection ->findOne(
+  ['_id' => 'teamC']
+);
+$option = $collection ->findOne(
+  ['_id' => 'option']
+);
+$time = $collection ->findOne(
+  ['_id' => 'timer']
+);
+$turn = $collection ->findOne(
+  ['_id' => 'turn']
+);
+$lock = $collection ->findOne(
+  ['_id' => 'lock']
+);
+
 if(isset($_POST['previous'])) {
-  $state = $collection ->findOne(
-    ['_id' => 'state']);
     $state = iterator_to_array($state);
     $result = $collection->updateOne(
       ['_id' => 'state'],
@@ -39,8 +65,6 @@ if(isset($_POST['play'])) {
 }
 
 if(isset($_POST['next'])) {
-  $state = $collection ->findOne(
-    ['_id' => 'state']);
     $state = iterator_to_array($state);
     $result = $collection->updateOne(
       ['_id' => 'state'],
@@ -61,63 +85,11 @@ if(isset($_POST['next'])) {
     );
 }
 
-if(isset($_POST['addA'])) {
-  $teamA = $collection ->findOne(
-    ['_id' => 'teamA']);
-    $teamA = iterator_to_array($teamA);
-    $result = $collection->updateOne(
-      ['_id' => 'teamA'],
-      ['$set' => ['value' => $teamA['value'] + 10]]
-    );
-  }
-
-if(isset($_POST['substractA'])) {
-  $teamA = $collection ->findOne(
-    ['_id' => 'teamA']);
-    $teamA = iterator_to_array($teamA);
-    $result = $collection->updateOne(
-      ['_id' => 'teamA'],
-      ['$set' => ['value' => $teamA['value'] - 10]]
-    );
-  }
-
-if(isset($_POST['addB'])) {
-  $teamB = $collection ->findOne(
-    ['_id' => 'teamB']);
-    $teamB = iterator_to_array($teamB);
-    $result = $collection->updateOne(
-      ['_id' => 'teamB'],
-      ['$set' => ['value' => $teamB['value'] + 10]]
-    );
-  }
-
-if(isset($_POST['substractB'])) {
-  $teamB = $collection ->findOne(
-    ['_id' => 'teamB']);
-    $teamB = iterator_to_array($teamB);
-    $result = $collection->updateOne(
-      ['_id' => 'teamB'],
-      ['$set' => ['value' => $teamB['value'] - 10]]
-    );
-  }
-
-if(isset($_POST['addC'])) {
-  $teamC = $collection ->findOne(
-    ['_id' => 'teamC']);
-    $teamC = iterator_to_array($teamC);
-    $result = $collection->updateOne(
-      ['_id' => 'teamC'],
-      ['$set' => ['value' => $teamC['value'] + 10]]
-    );
-  }
-
-if(isset($_POST['substractC'])) {
-$teamC = $collection ->findOne(
-  ['_id' => 'teamC']);
-  $teamC = iterator_to_array($teamC);
+if(isset($_POST['timer'])) {
+  $time = $_POST['timer'];
   $result = $collection->updateOne(
-    ['_id' => 'teamC'],
-    ['$set' => ['value' => $teamC['value'] - 10]]
+    ['_id' => 'timer'],
+    ['$set' => ['value' => $time]]
   );
 }
 
@@ -170,29 +142,29 @@ if(isset($_POST['option3in3'])) {
   );
 }
 
-if(isset($_POST['timer'])) {
-  $time = $_POST['timer'];
+if(isset($_POST['lock'])) {
+  $lock = $_POST['lock'];
   $result = $collection->updateOne(
-    ['_id' => 'timer'],
-    ['$set' => ['value' => $time]]
+    ['_id' => 'lock'],
+    ['$set' => ['value' => $lock]]
   );
 }
 
 if(isset($_POST['turn'])) {
-  $lock = $collection ->findOne(
-    ['_id' => 'lock']
-  );
-  if($lock == "false") {
+  $lock = iterator_to_array($lock);
+  if($lock['value'] == "false") {
     $turn = $_POST['turn'];
-    $result = $collection->updateOne(
-      ['_id' => 'turn'],
-      ['$set' => ['value' => [$turn[0],null]]]
-    );
-    if(substr($turn,1,1) != "") {
+    if(substr($turn,0,1) != "") {
       $result = $collection->updateOne(
         ['_id' => 'turn'],
-        ['$set' => ['value' => [$turn[0], $turn[1]]]]
+        ['$set' => ['value' => [$turn[0],null]]]
       );
+      if(substr($turn,1,1) != "") {
+        $result = $collection->updateOne(
+          ['_id' => 'turn'],
+          ['$set' => ['value' => [$turn[0], $turn[1]]]]
+        );
+      }
     }
   } else {
     $result = $collection->updateOne(
@@ -202,42 +174,53 @@ if(isset($_POST['turn'])) {
   }
 }
 
-if(isset($_POST['lock'])) {
-  $lock = $_POST['lock'];
+if(isset($_POST['addA'])) {
+  $teamA = iterator_to_array($teamA);
   $result = $collection->updateOne(
-    ['_id' => 'lock'],
-    ['$set' => ['value' => $lock]]
+    ['_id' => 'teamA'],
+    ['$set' => ['value' => $teamA['value'] + 10]]
   );
 }
 
-$play = $collection->findOne(
-  ['_id' => 'play']
-);
-$state = $collection->findOne(
-  ['_id' => 'state']
-);
-$teamA = $collection ->findOne(
-  ['_id' => 'teamA']
-);
-$teamB = $collection ->findOne(
-  ['_id' => 'teamB']
-);
-$teamC = $collection ->findOne(
-  ['_id' => 'teamC']
-);
-$option = $collection ->findOne(
-  ['_id' => 'option']
-);
-$time = $collection ->findOne(
-  ['_id' => 'timer']
-);
-$turn = $collection ->findOne(
-  ['_id' => 'turn']
-);
-$lock = $collection ->findOne(
-  ['_id' => 'lock']
-);
+if(isset($_POST['substractA'])) {
+  $teamA = iterator_to_array($teamA);
+  $result = $collection->updateOne(
+    ['_id' => 'teamA'],
+    ['$set' => ['value' => $teamA['value'] - 10]]
+  );
+}
+
+if(isset($_POST['addB'])) {
+  $teamB = iterator_to_array($teamB);
+  $result = $collection->updateOne(
+    ['_id' => 'teamB'],
+    ['$set' => ['value' => $teamB['value'] + 10]]
+  );
+}
+
+if(isset($_POST['substractB'])) {
+  $teamB = iterator_to_array($teamB);
+  $result = $collection->updateOne(
+    ['_id' => 'teamB'],
+    ['$set' => ['value' => $teamB['value'] - 10]]
+  );
+}
+
+if(isset($_POST['addC'])) {
+  $teamC = iterator_to_array($teamC);
+  $result = $collection->updateOne(
+    ['_id' => 'teamC'],
+    ['$set' => ['value' => $teamC['value'] + 10]]
+  );
+}
+
+if(isset($_POST['substractC'])) {
+  $teamC = iterator_to_array($teamC);
+  $result = $collection->updateOne(
+    ['_id' => 'teamC'],
+    ['$set' => ['value' => $teamC['value'] - 10]]
+  );
+}
 
 $jsono = array($play, $state, $option, $teamA, $teamB, $teamC, $time, $turn, $lock);
-
 echo json_encode($jsono);
