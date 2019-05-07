a6d57e1b4e5eacda281a551e400751d7a77acf6e
Luismoteando <LuisMiguel.Ortiz1@alu.uclm.es>
2019-05-07 15:42:30 +0200

    ninety-nineth commit

--- server/cyclServer.php
+++ server/cyclServer.php
@@ -8,41 +8,23 @@ $db = $client->olimpiada;
 $collection = $db->cycl;
 $dir = "/Applications/MAMP/htdocs/client/media/questions/cycl";
 
-$play = $collection->findOne(['_id' => 'play']);
-$mandatory = $collection->findOne(['_id' => 'mandatory']);
+$mode = $collection->findOne(['_id' => 'mode']);
 $lead = $collection->findOne(['_id' => 'lead']);
+$category = $collection->findOne(['_id' => 'category']);
+$mandatory = $collection->findOne(['_id' => 'mandatory']);
+$general = $collection->findOne(['_id' => 'general']);
+$play = $collection->findOne(['_id' => 'play']);
+$time = $collection->findOne(['_id' => 'timer']);
+$option = $collection->findOne(['_id' => 'option']);
+$solution = $collection->findOne(['_id' => 'solution']);
+$lock = $collection->findOne(['_id' => 'lock']);
+$turn = $collection->findOne(['_id' => 'turn']);
 $teamA = $collection->findOne(['_id' => 'teamA']);
 $teamB = $collection->findOne(['_id' => 'teamB']);
 $teamC = $collection->findOne(['_id' => 'teamC']);
-$option = $collection->findOne(['_id' => 'option']);
-$time = $collection->findOne(['_id' => 'timer']);
-$turn = $collection->findOne(['_id' => 'turn']);
-$lock = $collection->findOne(['_id' => 'lock']);
 $sfx = $collection->findOne(['_id' => 'sfx']);
-$solution = $collection->findOne(['_id' => 'solution']);
-$mode = $collection->findOne(['_id' => 'mode']);
-$category = $collection->findOne(['_id' => 'category']);
-$general = $collection->findOne(['_id' => 'general']);
 
-$mandatory = iterator_to_array($mandatory);
-if($mandatory['value'][1] == null) {
-  $total  = count(glob("$dir/mandatory/*"), GLOB_ONLYDIR);
-  $result = $collection->updateOne(
-    ['_id' => 'mandatory'],
-    ['$set' => ['value.1' => $total]]
-  );
-}
-
-$general = iterator_to_array($general);
-if($general['value'][1] == null) {
-  $total  = count(glob("$dir/general/*"), GLOB_ONLYDIR);
-  $result = $collection->updateOne(
-    ['_id' => 'general'],
-    ['$set' => ['value.1' => $total]]
-  );
-}
-
-if(isset($_POST['mode'])) {
+if (isset($_POST['mode'])) {
   $mode = $_POST['mode'];
   $result = $collection->updateOne(
     ['_id' => 'mode'],
@@ -50,7 +32,7 @@ if(isset($_POST['mode'])) {
   );
 }
 
-if(isset($_POST['lead'])) {
+if (isset($_POST['lead'])) {
   $lead = $_POST['lead'];
   $result = $collection->updateOne(
     ['_id' => 'lead'],
@@ -58,7 +40,7 @@ if(isset($_POST['lead'])) {
   );
 }
 
-if(isset($_POST['category'])) {
+if (isset($_POST['category'])) {
   $category = $_POST['category'];
   $result = $collection->updateOne(
     ['_id' => 'category'],
@@ -66,7 +48,16 @@ if(isset($_POST['category'])) {
   );
 }
 
-if(isset($_POST['mandatory'])) {
+$mandatory = iterator_to_array($mandatory);
+if ($mandatory['value'][1] == null) {
+  $total  = count(glob("$dir/mandatory/*"), GLOB_ONLYDIR);
+  $result = $collection->updateOne(
+    ['_id' => 'mandatory'],
+    ['$set' => ['value.1' => $total]]
+  );
+}
+
+if (isset($_POST['mandatory'])) {
   $mandatory = $_POST['mandatory'];
   $result = $collection->updateOne(
     ['_id' => 'mandatory'],
@@ -74,7 +65,7 @@ if(isset($_POST['mandatory'])) {
   );
 }
 
-if(isset($_POST['general'])) {
+if (isset($_POST['general'])) {
   $general = $_POST['general'];
   $result = $collection->updateOne(
     ['_id' => 'general'],
@@ -82,9 +73,18 @@ if(isset($_POST['general'])) {
   );
 }
 
-if(isset($_POST['previous'])) {
+$general = iterator_to_array($general);
+if ($general['value'][1] == null) {
+  $total  = count(glob("$dir/general/*"), GLOB_ONLYDIR);
+  $result = $collection->updateOne(
+    ['_id' => 'general'],
+    ['$set' => ['value.1' => $total]]
+  );
+}
+
+if (isset($_POST['previous'])) {
   $category = $_POST['category'];
-  if($category == "mandatory") {
+  if ($category == "mandatory") {
     $result = $collection->updateOne(
       ['_id' => 'mandatory'],
       ['$set' => ['value.0' => $mandatory['value'][0] - 1]]
@@ -102,7 +102,7 @@ if(isset($_POST['previous'])) {
       ['_id' => 'turn'],
       ['$set' => ['value' => [null, null, null]]]
     );
-  } elseif($category == "general") {
+  } elseif ($category == "general") {
     $result = $collection->updateOne(
       ['_id' => 'general'],
       ['$set' => ['value.0' => $general['value'][0] - 1]]
@@ -123,7 +123,7 @@ if(isset($_POST['previous'])) {
   }
 }
 
-if(isset($_POST['play'])) {
+if (isset($_POST['play'])) {
   $play = $_POST['play'];
   $result = $collection->updateOne(
     ['_id' => 'play'],
@@ -131,9 +131,9 @@ if(isset($_POST['play'])) {
   );
 }
 
-if(isset($_POST['next'])) {
+if (isset($_POST['next'])) {
   $category = $_POST['category'];
-  if($category == "mandatory") {
+  if ($category == "mandatory") {
     $result = $collection->updateOne(
       ['_id' => 'mandatory'],
       ['$set' => ['value.0' => $mandatory['value'][0] + 1]]
@@ -151,7 +151,7 @@ if(isset($_POST['next'])) {
       ['_id' => 'turn'],
       ['$set' => ['value' => [null, null, null]]]
     );
-  } elseif($category == "general") {
+  } elseif ($category == "general") {
     $result = $collection->updateOne(
       ['_id' => 'general'],
       ['$set' => ['value.0' => $general['value'][0] + 1]]
@@ -172,7 +172,7 @@ if(isset($_POST['next'])) {
   }
 }
 
-if(isset($_POST['timer'])) {
+if (isset($_POST['timer'])) {
   $time = $_POST['timer'];
   $result = $collection->updateOne(
     ['_id' => 'timer'],
@@ -180,56 +180,56 @@ if(isset($_POST['timer'])) {
   );
 }
 
-if(isset($_POST['option1in4'])) {
+if (isset($_POST['option1p4'])) {
   $result = $collection->updateOne(
     ['_id' => 'option'],
     ['$set' => ['value' => 14]]
   );
 }
 
-if(isset($_POST['option2in4'])) {
+if (isset($_POST['option2p4'])) {
   $result = $collection->updateOne(
     ['_id' => 'option'],
     ['$set' => ['value' => 24]]
   );
 }
 
-if(isset($_POST['option3in4'])) {
+if (isset($_POST['option3p4'])) {
   $result = $collection->updateOne(
     ['_id' => 'option'],
     ['$set' => ['value' => 34]]
   );
 }
 
-if(isset($_POST['option4in4'])) {
+if (isset($_POST['option4p4'])) {
   $result = $collection->updateOne(
     ['_id' => 'option'],
     ['$set' => ['value' => 44]]
   );
 }
 
-if(isset($_POST['option1in3'])) {
+if (isset($_POST['option1p3'])) {
   $result = $collection->updateOne(
     ['_id' => 'option'],
     ['$set' => ['value' => 13]]
   );
 }
 
-if(isset($_POST['option2in3'])) {
+if (isset($_POST['option2p3'])) {
   $result = $collection->updateOne(
     ['_id' => 'option'],
     ['$set' => ['value' => 23]]
   );
 }
 
-if(isset($_POST['option3in3'])) {
+if (isset($_POST['option3p3'])) {
   $result = $collection->updateOne(
     ['_id' => 'option'],
     ['$set' => ['value' => 33]]
   );
 }
 
-if(isset($_POST['solution'])) {
+if (isset($_POST['solution'])) {
   $solution = $_POST['solution'];
   $result = $collection->updateOne(
     ['_id' => 'solution'],
@@ -237,15 +237,7 @@ if(isset($_POST['solution'])) {
   );
 }
 
-if(isset($_POST['sfx'])) {
-  $sfx = $_POST['sfx'];
-  $result = $collection->updateOne(
-    ['_id' => 'sfx'],
-    ['$set' => ['value' => $sfx]]
-  );
-}
-
-if(isset($_POST['lock'])) {
+if (isset($_POST['lock'])) {
   $lock = $_POST['lock'];
   $result = $collection->updateOne(
     ['_id' => 'lock'],
@@ -253,32 +245,33 @@ if(isset($_POST['lock'])) {
   );
 }
 
-if(isset($_POST['turn'])) {
+if (isset($_POST['turn'])) {
   $lock = iterator_to_array($lock);
   $serial = $_POST['turn'];
   $turn = iterator_to_array($turn);
   $buffer = $turn['buffer'];
   $value = $turn['value'];
-  if($lock['value'] == 'false') {
+
+  if ($lock['value'] == 'false') {
     $buffer1 = str_split($serial, 1);
     $buffer2 = str_split($buffer, 1);
     $diff = array_diff_assoc($buffer1, $buffer2);
-    if($value[0] == null) {
-      if($first = reset($diff)) {
+    if ($value[0] == null) {
+      if ($first = reset($diff)) {
         $result = $collection->updateOne(
           ['_id' => 'turn'],
           ['$set' => ['value.0' => $first, 'buffer' => $serial]]
         );
       }
-    } elseif($value[1] == null) {
-      if($second = reset($diff)) {
+    } elseif ($value[1] == null) {
+      if ($second = reset($diff)) {
         $result = $collection->updateOne(
           ['_id' => 'turn'],
           ['$set' => ['value.1' => $second, 'buffer' => $serial]]
         );
       }
-    } elseif($value[2] == null) {
-      if($third = reset($diff)) {
+    } elseif ($value[2] == null) {
+      if ($third = reset($diff)) {
         $result = $collection->updateOne(
           ['_id' => 'turn'],
           ['$set' => ['value.2' => $third, 'buffer' => $serial]]
@@ -286,7 +279,7 @@ if(isset($_POST['turn'])) {
       }
     }
   } else {
-    if($value[0] == null) {
+    if ($value[0] == null) {
       $result = $collection->updateOne(
         ['_id' => 'turn'],
         ['$set' => ['value' => [null, null, null], 'buffer' => $serial]]
@@ -295,9 +288,9 @@ if(isset($_POST['turn'])) {
   }
 }
 
-if(isset($_POST['turnAux'])) {
+if (isset($_POST['turnAux'])) {
   $turnAux = $_POST['turnAux'];
-  if($turnAux != "") {
+  if ($turnAux != "") {
     $result = $collection->updateOne(
       ['_id' => 'turn'],
       ['$set' => ['value.0' => $turnAux]]
@@ -310,53 +303,82 @@ if(isset($_POST['turnAux'])) {
   }
 }
 
-if(isset($_POST['addA'])) {
+if (isset($_POST['team'])) {
+  $team = $_POST['team'];
+  $alias = $_POST['alias'];
+  if ($team == "teamA") {
+    $result = $collection->updateOne(
+      ['_id' => 'teamA'],
+      ['$set' => ['value.0' => $alias]]
+    );
+  } elseif ($team == "teamB") {
+    $result = $collection->updateOne(
+      ['_id' => 'teamB'],
+      ['$set' => ['value.0' => $alias]]
+    );
+  } elseif ($team == "teamC") {
+    $result = $collection->updateOne(
+      ['_id' => 'teamC'],
+      ['$set' => ['value.0' => $alias]]
+    );
+  }
+}
+
+if (isset($_POST['addA'])) {
   $teamA = iterator_to_array($teamA);
   $result = $collection->updateOne(
     ['_id' => 'teamA'],
-    ['$set' => ['value' => $teamA['value'] + 10]]
+    ['$set' => ['value.1' => $teamA['value'][1] + 10]]
   );
 }
 
-if(isset($_POST['substractA'])) {
+if (isset($_POST['subA'])) {
   $teamA = iterator_to_array($teamA);
   $result = $collection->updateOne(
     ['_id' => 'teamA'],
-    ['$set' => ['value' => $teamA['value'] - 10]]
+    ['$set' => ['value.1' => $teamA['value'][1] - 10]]
   );
 }
 
-if(isset($_POST['addB'])) {
+if (isset($_POST['addB'])) {
   $teamB = iterator_to_array($teamB);
   $result = $collection->updateOne(
     ['_id' => 'teamB'],
-    ['$set' => ['value' => $teamB['value'] + 10]]
+    ['$set' => ['value.1' => $teamB['value'][1] + 10]]
   );
 }
 
-if(isset($_POST['substractB'])) {
+if (isset($_POST['subB'])) {
   $teamB = iterator_to_array($teamB);
   $result = $collection->updateOne(
     ['_id' => 'teamB'],
-    ['$set' => ['value' => $teamB['value'] - 10]]
+    ['$set' => ['value.1' => $teamB['value'][1] - 10]]
   );
 }
 
-if(isset($_POST['addC'])) {
+if (isset($_POST['addC'])) {
   $teamC = iterator_to_array($teamC);
   $result = $collection->updateOne(
     ['_id' => 'teamC'],
-    ['$set' => ['value' => $teamC['value'] + 10]]
+    ['$set' => ['value.1' => $teamC['value'][1] + 10]]
   );
 }
 
-if(isset($_POST['substractC'])) {
+if (isset($_POST['subC'])) {
   $teamC = iterator_to_array($teamC);
   $result = $collection->updateOne(
     ['_id' => 'teamC'],
-    ['$set' => ['value' => $teamC['value'] - 10]]
+    ['$set' => ['value.1' => $teamC['value'][1] - 10]]
   );
 }
 
-$jsono = array($play, $mandatory, $option, $teamA, $teamB, $teamC, $time, $turn, $lock, $sfx, $solution, $mode, $lead, $category, $general);
+if (isset($_POST['sfx'])) {
+  $sfx = $_POST['sfx'];
+  $result = $collection->updateOne(
+    ['_id' => 'sfx'],
+    ['$set' => ['value' => $sfx]]
+  );
+}
+
+$jsono = array($mode, $lead, $category, $mandatory, $general, $play, $time, $option, $solution, $lock, $turn, $teamA, $teamB, $teamC, $sfx, );
 echo json_encode($jsono);
