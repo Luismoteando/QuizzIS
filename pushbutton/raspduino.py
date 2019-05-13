44d2bfb07ff8391ec9b6b294aa4d89b91a4fbdd0
Luismoteando <LuisMiguel.Ortiz1@alu.uclm.es>
2019-05-13 18:10:00 +0200

    hundred-seventh commit

--- pushbutton/raspduino.py
+++ pushbutton/raspduino.py
@@ -14,7 +14,7 @@ def receiving(ser):
             lines = buffer_string.split('\n')
             last_received = lines[-2]
             buffer_string = lines[-1]
-        requests.post(url = "http://169.254.215.26:8888/server/bachServer.php", data = {'turn' : buffer_string})
+        requests.post(url = "http://192.168.0.8:8888/server/bachServer.php", data = {'turn' : buffer_string})
         # if len(buffer_string) > 2:
         #     buffer_string = ''
 
