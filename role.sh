b28cb38119db8d8426d1a16a2af1c5123a28c668
Luismoteando <LuisMiguel.Ortiz1@alu.uclm.es>
2019-05-21 15:50:34 +0200

    hundred-sixteenth commit

--- role.sh
+++ role.sh
@@ -7,10 +7,10 @@ done
 
 while IFS=" " read -r role team; do
   if [[ $role == "videoplayer" ]]; then
-    /usr/bin/chromium-browser --kiosk --disable-restore-session-state --app="http://localhost/client/videoplayer.html"
+    /usr/bin/chromium-browser --kiosk --disable-restore-session-state --app="http://192.168.0.8:8888/client/videoplayer.html"
   elif [[ $role == "videomarker" ]]; then
     if [[ $team == "teamA" || $team == "teamB" || $team == "teamC" ]]; then
-      /usr/bin/chromium-browser --kiosk --disable-restore-session-state --app="http://localhost/client/videomarker.html?team=$team"
+      /usr/bin/chromium-browser --kiosk --disable-restore-session-state --app="http://192.168.8:8888/client/videomarker.html?team=$team"
     fi
   fi
 done < "$file"
